17fd07580f9f167d3337356c54c23449334b1dc9
Nikitushka23 <unclevader23@gmail.com>
2020-08-06 17:04:52 +0300

    Updates

---
+++ precache-manifest.fe13e19a32d9a7295ec8109667e41dc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdce395e9241e36e1a853867f932cc98",
+    "url": "/covidInfoReactJs/index.html"
+  },
+  {
+    "revision": "06e83b74e2a1084c7ba5",
+    "url": "/covidInfoReactJs/static/css/main.462f484d.chunk.css"
+  },
+  {
+    "revision": "91b26888304d0e6c8b9a",
+    "url": "/covidInfoReactJs/static/js/2.1afa92dd.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covidInfoReactJs/static/js/2.1afa92dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e83b74e2a1084c7ba5",
+    "url": "/covidInfoReactJs/static/js/main.4e58da9d.chunk.js"
+  },
+  {
+    "revision": "d14b7eb3d33feedf39c2",
+    "url": "/covidInfoReactJs/static/js/runtime-main.ebc15dd4.js"
+  }
+]);
